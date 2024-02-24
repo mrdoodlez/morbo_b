4141300da71b7454dfeccd343a1282c7b3ccebdc
mrdoodlez <doodlez1984@gmail.com>
2024-02-24 20:19:35 +0100

    add FreeRTOS

--- stm32_fw/main/Sources/stm32g4xx_it.h
+++ stm32_fw/main/Sources/stm32g4xx_it.h
@@ -49,9 +49,6 @@
 /* Exported functions prototypes ---------------------------------------------*/
 void NMI_Handler(void);
 void HardFault_Handler(void);
-void SVC_Handler(void);
-void PendSV_Handler(void);
-void SysTick_Handler(void);
 void USART1_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 void EXTI15_10_IRQHandler(void);
